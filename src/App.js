b53bd734d1ec04f0712dae8e461366e804e87cd3
SLFireFlash <sllahiruprasanna@gmail.com>
2023-02-06 12:56:07 +0530

    added rouetes

--- src/App.js
+++ src/App.js
@@ -1,59 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from "./post";
+import Header from "./header";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-<main>
-  <header>
-    <a herf="" className='logo'>TeamHiru Blog</a>
-    <nav>
-      <a href=''>LOG IN</a>
-      <a href=''>SIGN UP</a>
-    </nav>
-  </header>
-  <div className="post">
-    <div className="image">
-      <img src="https://www.freecodecamp.org/news/content/images/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt="#" />
-    </div>
-    <div className="texts">
-      <h2>Welcome To my Blog</h2>
-      <p className="info">
-        <a className="author">HIRU</a>
-        <time>2023-02-06</time>
-      </p>
-      <p className="summary">
-      Apex Legends is available for free on 
-      Ps4, Ps5, Xbox One, Xbox Series X|S,
-      Nintendo Switch, and PC via EA App and Steam.
-      Choose your platform below, download the game, 
-      and get ready to jump into the arena.
-      </p>
-    </div>
-  </div>
-{/* end of the post */}
-
-<div className="post">
-    <div className="image">
-      <img src="https://www.freecodecamp.org/news/content/images/2022/09/jonatan-pie-3l3RwQdHRHg-unsplash.jpg" alt="#" />
-    </div>
-    <div className="texts">
-      <h2>Welcome To my Blog</h2>
-      <p className="info">
-        <a className="author">HIRU</a>
-        <time>2023-02-06</time>
-      </p>
-      <p className='summary'>
-      Apex Legends is available for free on 
-      Ps4, Ps5, Xbox One, Xbox Series X|S,
-      Nintendo Switch, and PC via EA App and Steam.
-      Choose your platform below, download the game, 
-      and get ready to jump into the arena.
-      </p>
-    </div>
-  </div>
-  
-  
-</main>
+  <Routes>
+    <Route index element={
+      <main>
+          <Header/>
+          <Post />
+          <Post />
+      </main>
+    } />
+  </Routes>
   );
 }
 
---
+++ src/header.js
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+        <a herf="" className='logo'>TeamHiru Blog</a>
+        <nav>
+          <a href=''>LOG IN</a>
+          <a href=''>SIGN UP</a>
+        </nav>
+      </header>
+    );
+}
